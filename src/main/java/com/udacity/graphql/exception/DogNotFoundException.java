4993127a9ee2edeec7943338c2065047f45c0277
Chizzy Ibemeru <ibemeruchizzy@gmail.com>
2022-07-06 23:13:35 +0100

    Add files via upload

---
+++ src/main/java/com/udacity/graphql/exception/DogNotFoundException.java
@@ -0,0 +1,43 @@
+package com.udacity.graphql.exception;
+
+import graphql.ErrorClassification;
+import graphql.ErrorType;
+import graphql.GraphQLError;
+import graphql.language.SourceLocation;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DogNotFoundException extends RuntimeException implements GraphQLError {
+    private Map<String, Object> extentions = new HashMap<>();
+
+    public DogNotFoundException(String message, Long invalidDogId){
+        super(message);
+        extentions.put("invalidDogId", invalidDogId);
+    }
+    @Override
+    public List<SourceLocation> getLocations() {
+        return null;
+    }
+
+    @Override
+    public ErrorClassification getErrorType() {
+        return ErrorType.DataFetchingException;
+    }
+
+    @Override
+    public List<Object> getPath() {
+        return GraphQLError.super.getPath();
+    }
+
+    @Override
+    public Map<String, Object> toSpecification() {
+        return GraphQLError.super.toSpecification();
+    }
+
+    @Override
+    public Map<String, Object> getExtensions() {
+        return extentions;
+    }
+}
